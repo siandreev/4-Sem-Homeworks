ee168a9758df3ef1c309de2c36522caed7d9faa3
Sergey <sera200013@gmail.com>
2019-02-22 16:59:05 +0300

    Added task 1

---
+++ 1 Fact/1 FactoriaL/1 FactoriaL/Program.fs
@@ -0,0 +1,13 @@
+﻿let x = System.Console.ReadLine() |>int
+
+let rec factorial n acc =
+    try
+        match n with
+        | n when (n < 0) || (n > 33) -> failwith "Incorrect nomber"
+        | 0 -> acc
+        | _ -> factorial (n - 1) (acc * n)
+    with :? System.Exception as ex-> printfn "Exception %s" (ex.Message); -1
+let fact n = factorial n 1
+printf "%A" (fact x)
+    
+

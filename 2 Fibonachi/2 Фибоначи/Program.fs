4f1329479f257d1c4f00922732ef8f0debbdf984
Sergey <sera200013@gmail.com>
2019-02-27 00:02:22 +0300

    task 2 fixed

--- 2 Fibonachi/2 Фибоначи/Program.fs
+++ 2 Fibonachi/2 Фибоначи/Program.fs
@@ -1,15 +1,14 @@
 ﻿let x = System.Console.ReadLine() |> int 
 
-
-
-let rec fibonachi n acc1 acc2 = 
-    try
-        match n with
-        | n when n < 0 -> failwith "Incorrect nomber"
-        | 0 -> acc2
-        | _ -> fibonachi (n - 1) acc2 (acc1 + acc2)
-    with :? System.Exception as ex-> printfn "Exception %s" (ex.Message); -1
-let fib n = fibonachi n 0 1 
+let fib n =
+    let rec fibonachi n acc1 acc2 = 
+        try
+            match n with
+            | n when n < 0 -> failwith "Incorrect nomber"
+            | 0 -> acc2
+            | _ -> fibonachi (n - 1) acc2 (acc1 + acc2)
+        with :? System.Exception as ex-> printfn "Exception %s" (ex.Message); -1
+    fibonachi n 0 1 
 printfn "%A"(fib x)
 
 

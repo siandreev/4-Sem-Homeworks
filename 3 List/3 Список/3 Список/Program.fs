e7cf511b2011421da4c3660d5fa3f9f0db345c4a
Sergey <sera200013@gmail.com>
2019-02-27 00:04:28 +0300

    task 3 fixed

--- 3 List/3 Список/3 Список/Program.fs
+++ 3 List/3 Список/3 Список/Program.fs
@@ -1,9 +1,9 @@
 ﻿let CurrentList = [1;2;3;4;5;6;7;8;9;10]
 
-let rec reverse Hvost AccList = 
-    match Hvost with
-    | (x::xs)  -> reverse xs (x::AccList)
-    | [] -> AccList
-    
-let rev list1 = reverse list1 []
+let rev list1 = 
+    let rec reverse Hvost AccList = 
+        match Hvost with
+        | (x::xs)  -> reverse xs (x::AccList)
+        | [] -> AccList
+    reverse list1 []
 printf "%A" (rev CurrentList)

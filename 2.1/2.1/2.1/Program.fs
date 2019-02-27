9e80b79186c0b7ee01dfaf880c1e8b86a9375970
Sergey <sera200013@gmail.com>
2019-02-27 21:06:13 +0300

    task 2.1 fixed

--- 2.1/2.1/2.1/Program.fs
+++ 2.1/2.1/2.1/Program.fs
@@ -2,14 +2,14 @@
 let li = [1;2;3;4;5]
 let N = System.Console.ReadLine() |> int
 
-let ch list1 template =
+let compare list1 template =
     let rec check list1 template count =
-        let compare a b =
+        let ch a b =
             if a=b then true
             else false
         match list1 with
-        | x::xs -> compare x template ; if not (compare x template) then check xs template (count + 1) else Some(count)
+        | x::xs -> ch x template ; if not (ch x template) then check xs template (count + 1) else Some(count)
         | [] -> None
     check list1 template 1
-printf "%A" (ch li N )
+printf "%A" (compare li N )
 System.Console.ReadKey();

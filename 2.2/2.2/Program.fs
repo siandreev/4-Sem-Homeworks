0fc18de9c814a1dcecf9e2ebdd0abf3c260d1ee7
Sergey <sera200013@gmail.com>
2019-02-27 21:09:53 +0300

    task 2.2 fixed

--- 2.2/2.2/Program.fs
+++ 2.2/2.2/Program.fs
@@ -1,11 +1,11 @@
 ﻿let str1 = System.Console.ReadLine();
 
-let ch str=
-    let rec check str n = 
+let check str=
+    let rec ch str n = 
         match str with
         |_ when n >= String.length(str) - n - 1 -> true
-        |_ when str.[n] = str.[String.length(str) - n - 1] -> check str (n+1)
+        |_ when str.[n] = str.[String.length(str) - n - 1] -> ch str (n+1)
         |_ when str.[n] <> str.[String.length(str) - n - 1] -> false
-    check str 0
-printf "%A" (ch str1);
+    ch str 0
+printf "%A" (check str1);
 System.Console.ReadKey();

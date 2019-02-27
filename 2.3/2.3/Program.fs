96cdd2d22aebab5643db9eff312b22a12a57b333
Sergey <sera200013@gmail.com>
2019-02-27 19:15:36 +0300

    Added task 2.3

---
+++ 2.3/2.3/Program.fs
@@ -0,0 +1,6 @@
+﻿let rec sort list1 =
+    match list1 with
+    | [ ] -> [ ] 
+    | x::xs -> sort(List.filter(fun n -> n < x) xs) @ [x] @ sort(List.filter (fun n -> n >= x) xs) 
+printf "%A" (sort [3;2;1;4])
+System.Console.ReadKey()

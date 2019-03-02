d5aa5642888f184bed172767fbca9b9140be8ec7
Sergey <sera200013@gmail.com>
2019-03-02 14:48:05 +0300

    Added tests

---
+++ 2.2/2.2.Tests/TestsProgram2.2.fs
@@ -0,0 +1,22 @@
+﻿module tests 
+    
+    open NUnit.Framework
+    open FsUnit
+    open logic
+
+    [<Test>]
+    let ``String "abccba" is palindrome that is why result should be true`` () =
+        checkPalindrome "abccba" |> should equal true
+
+    [<Test>]
+    let ``String "abcba" is palindrome that is why result should be true`` () =
+        checkPalindrome "abcba" |> should equal true
+
+    
+    [<Test>]
+    let ``String "abcdba" is not palindrome that is why result should be false`` () =
+        checkPalindrome "abcdba" |> should equal false
+
+    [<Test>]
+    let ``Empty string is palindrome that is why result should be true`` () =
+        checkPalindrome "" |> should equal true 
---
+++ 2.2/2.2/Program2.2.fs
@@ -0,0 +1,9 @@
+﻿module logic
+
+    let checkPalindrome str=
+        let rec ch str n = 
+            match str with
+            |_ when n >= String.length(str) - n - 1 -> true
+            |_ when str.[n] = str.[String.length(str) - n - 1] -> ch str (n+1)
+            |_ when str.[n] <> str.[String.length(str) - n - 1] -> false
+        ch str 0

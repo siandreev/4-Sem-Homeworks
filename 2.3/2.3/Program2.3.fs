085f471a361237bb1635ae1f4d14aacfd516d8e7
sergeyandreev239 <sera200013@gmail.com>
2019-03-02 15:10:42 +0300

    Revert "Homework 2.3"

--- 2.3/2.3/Program2.3.fs
+++
@@ -1,5 +0,0 @@
-﻿module logic
-    let rec sort list1 =
-        match list1 with
-        | [ ] -> [ ] 
-        | x::xs -> sort(List.filter(fun n -> n < x) xs) @ [x] @ sort(List.filter (fun n -> n >= x) xs) 

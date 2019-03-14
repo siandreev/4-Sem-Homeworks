ce2b8f082f36d5392e8c10a338f62daf3aaf4d7a
Sergey <sera200013@gmail.com>
2019-03-15 00:51:18 +0300

    task 3.3 added

---
+++ 3.3Tests/3.3Tests.Program.fs
@@ -0,0 +1,16 @@
+﻿module tests
+    open logic
+    open FsUnit
+    open NUnit.Framework
+
+    [<Test>]
+    let ``Сalculate 2 + 2`` () =
+        eval (Addition(Number 2, Number 2)) |> should equal 4
+
+    [<Test>]
+    let ``Сalculate (7 + 5) * 2 / 3 - 2`` () =
+        eval (Subtraction(Division(Multiplications(Addition(Number 7, Number 5),Number 2), Number 3), Number 2)) |> should equal 6
+
+    [<Test>]
+    let ``Output 0`` () =
+        eval (Number 0) |> should equal 0

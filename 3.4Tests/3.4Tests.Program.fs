24ae4b491f2f76b998e4febb8f574872d0ef010a
Sergey <sera200013@gmail.com>
2019-03-15 17:16:32 +0300

    task 3.4 added

---
+++ 3.4Tests/3.4Tests.Program.fs
@@ -0,0 +1,16 @@
+﻿module tests
+    open logic
+    open FsUnit
+    open NUnit.Framework
+
+    [<Test>]
+    let ``Check if seq cintains prime num 7`` () = 
+        Seq.exists (fun x -> x = 7) (createPrimesSeq())  |> should equal true
+
+    [<Test>]
+    let ``Check if seq contains prime num 239`` () = 
+        Seq.exists (fun x -> x = 239) (createPrimesSeq()) |> should equal true
+
+    [<Test>]
+    let ``Let's see 24th prime number`` () = 
+        createPrimesSeq () |> Seq.skip 23 |> Seq.take 1 |> Seq.toArray |> should equal [|89|]

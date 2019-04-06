5af95959ca2206f5d56f3de91456758457841ea0
Sergey <sera200013@gmail.com>
2019-04-06 14:19:13 +0300

    task 5.2 added

---
+++ 5.2/5.2/5.2.Tests/5.2.TestsProgram.fs
@@ -0,0 +1,11 @@
+﻿module tests
+    open logic
+    open FsCheck
+    open NUnit.Framework
+    
+    [<Test>]
+    let ``QuickCheckk``()=
+        Check.Quick(fun x l -> (func x l) = (func'3 x l)) 
+
+
+ 
---
+++ 5.2/5.2/5.2/5.2Program.fs
@@ -0,0 +1,11 @@
+﻿module logic
+     let func x l = List.map (fun y -> y * x) l
+
+     let func'1 x : (int) list -> (int) list =
+        List.map(fun y -> y * x)
+    
+     let func'2 x : (int) list -> (int) list =
+        List.map((*) x )
+
+     let func'3 : int -> int list -> int list = 
+        (|>) List.map ((>>)(*))

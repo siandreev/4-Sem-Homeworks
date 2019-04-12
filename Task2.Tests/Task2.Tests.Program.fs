f4c4758f04ca125e1ab69bba9f1c69591eb16468
Sergey <sera200013@gmail.com>
2019-04-12 18:58:35 +0300

    Task2 added

---
+++ Task2.Tests/Task2.Tests.Program.fs
@@ -0,0 +1,20 @@
+﻿module Tests
+    open Logic
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``check 3 line in 4 by 4 square``() =
+        printString 4 3 |> should equal "*  *"
+
+    [<Test>]
+    let ``check 2 line in 5 by 5 square``() =
+        printString 5 2 |> should equal "*   *"
+
+    [<Test>]
+    let ``check 1 line in 3 by 3 square``() =
+        printString 3 1 |> should equal "***"
+
+    [<Test>]
+    let ``check 6 line in 6 by 6 square``() =
+        printString 6 6 |> should equal "******"
---
+++ Task2/Task2/Task2.Program.fs
@@ -0,0 +1,32 @@
+﻿module Logic    
+    open System
+    //создание полной строки
+    let rec printFullLine n acc stringAcc =
+            match acc with
+            | _ when acc = n + 1 -> stringAcc
+            | _ ->  
+                printFullLine n (acc + 1) (stringAcc + "*")
+    // создание дырявой строки
+    let rec printHoleLine n acc stringAcc=
+           match acc with
+           | _ when acc = 1 -> printHoleLine n (acc + 1) (stringAcc + "*")
+           | _ when acc = n -> printHoleLine n (acc + 1) (stringAcc + "*")
+           | _ when acc = n + 1 -> stringAcc
+           | _ -> printHoleLine n (acc + 1) (stringAcc + " ")
+    // создание произвольной строки по номеру
+    let rec printString n acc =
+           match acc with
+           | _ when acc = 1 -> (printFullLine n 1 "") 
+           | _ when acc = n -> (printFullLine n 1 "") 
+           | _ when acc = n + 1 -> ""
+           | _ -> (printHoleLine n 1 "") 
+    // печать всей картинки
+    let printSquare n =      
+        let rec printAllPicture n acc =
+            match acc with
+            | _ when acc <= n ->
+                Console.WriteLine(printString n acc)
+                printAllPicture n (acc + 1)
+            | _ when acc = n + 1 -> Console.Write("")
+        printAllPicture n 1
+            

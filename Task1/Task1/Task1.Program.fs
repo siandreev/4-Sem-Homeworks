e62ae9e837858e39fc1b01564fc98c2abc52e5e4
Sergey <sera200013@gmail.com>
2019-04-12 18:05:24 +0300

    Task1 added

---
+++ Task1/Task1/Task1.Program.fs
@@ -0,0 +1,16 @@
+﻿module Logic
+
+    open System
+
+    let sumOfEvenFibonacci() =
+        let rec fib acc1 acc2 sum =
+            match acc2 with 
+            | _ when acc2 >= 1000000 -> sum
+            | _ -> 
+                match acc2 with 
+                | _ when acc2 % 2 = 0 ->fib acc2 (acc1 + acc2) sum + acc2
+                | _ -> fib acc2 (acc1 + acc2) sum
+        fib 1 1 0
+
+    printf "%A" sumOfEvenFibonacci
+            

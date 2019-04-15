5484e669ee2d877cdfd920ec3d0900cfaaf2a77d
Sergey <sera200013@gmail.com>
2019-04-15 23:10:46 +0300

    StyleGuid fixed

--- 2.2/2.2.Tests/TestsProgram2.2.fs
+++ 2.2/2.2.Tests/TestsProgram2.2.fs
@@ -1,22 +1,22 @@
-﻿module tests 
+﻿module Tests 
     
     open NUnit.Framework
     open FsUnit
-    open logic
+    open Logic
 
     [<Test>]
     let ``String "abccba" is palindrome that is why result should be true`` () =
-        checkPalindrome "abccba" |> should equal true
+        checkIsPalindrome "abccba" |> should equal true
 
     [<Test>]
     let ``String "abcba" is palindrome that is why result should be true`` () =
-        checkPalindrome "abcba" |> should equal true
+        checkIsPalindrome "abcba" |> should equal true
 
     
     [<Test>]
     let ``String "abcdba" is not palindrome that is why result should be false`` () =
-        checkPalindrome "abcdba" |> should equal false
+        checkIsPalindrome "abcdba" |> should equal false
 
     [<Test>]
     let ``Empty string is palindrome that is why result should be true`` () =
-        checkPalindrome "" |> should equal true 
+        checkIsPalindrome "" |> should equal true 
--- 2.2/2.2/Program2.2.fs
+++ 2.2/2.2/Program2.2.fs
@@ -1,9 +1,9 @@
-﻿module logic
+﻿module Logic
 
-    let checkPalindrome str=
-        let rec ch str n = 
+    let checkIsPalindrome str=
+        let rec recChecking str n = 
             match str with
-            |_ when n >= String.length(str) - n - 1 -> true
-            |_ when str.[n] = str.[String.length(str) - n - 1] -> ch str (n+1)
-            |_ when str.[n] <> str.[String.length(str) - n - 1] -> false
-        ch str 0
+            | _ when n >= String.length(str) - n - 1 -> true
+            | _ when str.[n] = str.[String.length(str) - n - 1] -> recChecking str (n+1)
+            | _ when str.[n] <> str.[String.length(str) - n - 1] -> false
+        recChecking str 0

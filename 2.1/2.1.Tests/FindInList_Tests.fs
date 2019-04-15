9249c69adbd1d8902a6158c63fc616f37928d19c
Sergey <sera200013@gmail.com>
2019-04-15 22:52:52 +0300

    StyleGuid fixed

--- 2.1/2.1.Tests/FindInList_Tests.fs
+++ 2.1/2.1.Tests/FindInList_Tests.fs
@@ -1,20 +1,17 @@
-﻿module tests
+﻿module Tests
 
+    open NUnit.Framework
+    open FsUnit
+    open Logic
 
-open NUnit.Framework
-open FsUnit
-open logic
+    [<Test>]
+    let ``First appereance of 5 in list should be equal 4.`` () =
+        getFirstPosition [1..10] 5 |> should equal (Some(5))
 
+    [<Test>]
+    let ``First appereance of 4 in list should be equal None.`` () =
+        getFirstPosition [1..3] 4 |> should equal (None)
 
-
-[<Test>]
-let ``First appereance of 5 in list should be equal 4.`` () =
-    check [1..10] 5 |> should equal (Some(5))
-
-[<Test>]
-let ``First appereance of 4 in list should be equal None.`` () =
-    check [1..3] 4 |> should equal (None)
-
-[<Test>]
-let ``First appereance of 5 in empty list should be equal None.`` () =
-    check [] 5 |> should equal None 
+    [<Test>]
+    let ``First appereance of 5 in empty list should be equal None.`` () =
+        getFirstPosition [] 5 |> should equal None 
--- 2.1/2.1/FindInList.fs
+++ 2.1/2.1/FindInList.fs
@@ -1,17 +1,10 @@
-﻿
-module logic
-
-  //  let li = [1;2;3;4;5]
-   // let N = System.Console.ReadLine() |> int 
+﻿module Logic
     
-    let check list1 template =
-        let rec ch list1 template count =
-            let compare a b =
-                if a=b then true
-                else false
-            match list1 with
-            | x::xs -> compare x template ; if not (compare x template) then ch xs template (count + 1) else Some(count)
+    let getFirstPosition list template =
+        let rec recSearch list template count =
+            match list with
+            | x::xs -> 
+                if not (x = template) then recSearch xs template (count + 1) else Some(count)
             | [] -> None
-        ch list1 template 1
-   // printf "%A" (check li N )
-   // System.Console.ReadKey();
+        recSearch list template 1
+  

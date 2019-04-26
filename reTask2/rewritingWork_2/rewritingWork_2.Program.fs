98c2e70cee5bc4710a200f93ee804117d2eee250
Sergey <sera200013@gmail.com>
2019-04-26 18:10:58 +0300

    comments added

--- reTask2/rewritingWork_2/rewritingWork_2.Program.fs
+++ reTask2/rewritingWork_2/rewritingWork_2.Program.fs
@@ -1,4 +1,5 @@
 ﻿module Logic    
+    // проверка, является ли строка палиндромом
     let palindromeCheck (x : int)=
         let rec check str n = 
             if n >= String.length(str) - n - 1 then true
@@ -7,14 +8,15 @@
                 else false
         check (string x) 0
 
+    // перебираем все пары трехзначных чисел, храня максимальный на данный момент палиндром. Если находим больше- меняем. Когда переберем все пары- возвращаем максимум.
     let searchPalindrome() =
         let rec multplyiAndCheck first second maxPalindrome =
             match first with
+            | _ when (first = 99) -> maxPalindrome
             | _ when palindromeCheck (first*second) ->
                 if first*second > maxPalindrome then multplyiAndCheck first (second - 1) (first*second)
                 else multplyiAndCheck first (second - 1) maxPalindrome
             | _ when (second = 100) -> multplyiAndCheck (first - 1) 999 maxPalindrome
-            | _ when (first = 100) -> maxPalindrome
             | _ -> multplyiAndCheck first (second - 1) maxPalindrome
         multplyiAndCheck 999 999 0
 

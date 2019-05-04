a7d847d766764a901c06b29edf572856a6ea0164
Sergey <sera200013@gmail.com>
2019-05-05 00:16:10 +0300

    task 7.2 added

---
+++ 7.2/7.2/7.2.Program.fs
@@ -0,0 +1,12 @@
+﻿module Logic
+
+    type StringCalcBuilder() =
+        member this.Bind(s : string, f) =
+            try
+                f (int s)
+            with
+                :? System.FormatException -> printf "Wrong data"; None
+        member this.Return(s) =
+            Some(s)
+       
+           

dd62f566e31e838930311f970074d8bf052b67bf
Sergey <sera200013@gmail.com>
2019-05-04 23:37:49 +0300

    task 7.1 added

---
+++ 7.1.Tests/7.1.Test.Program.fs
@@ -0,0 +1,23 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``check example from task``()=
+        let res = RoundingBuilder 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        res |> should equal  0.048
+
+    [<Test>]
+    let ``check example from task with 15 signs``()=
+        let res = RoundingBuilder 15 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        res |> should equal  0.047619047619047998
---
+++ 7.1/7.1/7.1.Program.fs
@@ -0,0 +1,11 @@
+﻿module Logic
+
+    type RoundingBuilder (precision : int) =
+        member this.Bind(x, f) =
+            f x
+        member this.Return (x : float) =
+            System.Math.Round(x, precision)
+
+   
+
+

267f4fd371ffd4bedb8f7646ad5240944d1a1abf
Sergey <sera200013@gmail.com>
2019-06-02 01:15:21 +0300

    MergeSort fixed

--- 2.3/2.3.Tests/TestsProgram2.3.fs
+++ 2.3/2.3.Tests/TestsProgram2.3.fs
@@ -6,12 +6,12 @@
 
     [<Test>]
     let ``sort 1 with list [3; 1; 2; 4] should give out [1; 2; 3; 4]`` () =
-        sortList [3; 1; 2; 4] |> should equal [1; 2; 3; 4]
+        mergeSort [3; 1; 2; 4] |> should equal [1; 2; 3; 4]
 
     [<Test>]
     let ``sort 2 with list [-2; -3; -2] should give out [-3; -2; -2]`` () =
-        sortList [-2; -3; -2] |> should equal [-3; -2; -2]
+        mergeSort [-2; -3; -2] |> should equal [-3; -2; -2]
 
     [<Test>]
     let ``sort 3 with empty list should give out []`` () =
-        sortList [] |> should equal [] 
+        mergeSort [] |> should equal [] 

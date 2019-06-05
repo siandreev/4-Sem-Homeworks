d8cbf26b6a90ca5d2ab3bfaeaaacda360293e40a
Sergey <sera200013@gmail.com>
2019-06-05 23:38:29 +0300

    Task 8.2 added

---
+++ 8.2/8.2.Tests/8.2.Tests.Program.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+    open Logic
+    open FsUnit
+    open NUnit.Framework
+
+    [<Test>]
+    let ``check count of references in some site``() =
+        let url = "https://stackoverflow.com/questions/20714492/antlr4-listeners-and-visitors-which-to-implement"
+        (webFunction url).Length |> should equal 3
+
+    [<Test>]
+    let ``check another site``() =
+        let url = "https://www.anekdot.ru/tags/%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0"
+        (webFunction url).Length |> should equal 2 
---
+++ 8.2/8.2/8.2.Program.fs
@@ -0,0 +1,25 @@
+﻿module Logic        
+
+    open System.Net
+    open System.Text.RegularExpressions
+    
+    let webFunction (url : string) =
+        let getInfoAsync (url : string)=
+            async {
+                    let req = WebRequest.Create(url) 
+                    use! resp = req.AsyncGetResponse() 
+                    use stream = resp.GetResponseStream() 
+                    use reader = new System.IO.StreamReader(stream)
+                    let html = reader.ReadToEnd()
+                    do printfn "url: %s -symbols count %d" url html.Length
+                    return html               
+            }
+        let sourceText = Async.RunSynchronously <| getInfoAsync url
+        let regex = Regex("<a href\s*=\s*\"?https?://[^\"]*\"?\s*>")
+        let dataList = [for href in regex.Matches(sourceText) -> 
+            let index1 = href.Value.IndexOf("\"")
+            let index2 = href.Value.LastIndexOf("\"")
+            href.Value.Substring(index1 + 1, index2 - index1 - 1)] |> Seq.distinct |> Seq.toList |>
+            List.map(fun x -> x |> getInfoAsync)
+        Async.Parallel dataList |> Async.RunSynchronously  
+        

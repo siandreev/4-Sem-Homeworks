33b1fe44373dbbe4fa7fb79b83778627387181d7
Sergey <sera200013@gmail.com>
2019-06-06 13:59:20 +0300

    warning fixed

--- 8.2/8.2/8.2.Program.fs
+++ 8.2/8.2/8.2.Program.fs
@@ -16,10 +16,12 @@
             }
         let sourceText = Async.RunSynchronously <| getInfoAsync url
         let regex = Regex("<a href\s*=\s*\"?https?://[^\"]*\"?\s*>")
-        let dataList = [for href in regex.Matches(sourceText) -> 
-            let index1 = href.Value.IndexOf("\"")
-            let index2 = href.Value.LastIndexOf("\"")
-            href.Value.Substring(index1 + 1, index2 - index1 - 1)] |> Seq.distinct |> Seq.toList |>
-            List.map(fun x -> x |> getInfoAsync)
+        let dataList = 
+            [for href in regex.Matches(sourceText) -> 
+                let index1 = href.Value.IndexOf("\"")
+                let index2 = href.Value.LastIndexOf("\"")
+                href.Value.Substring(index1 + 1, index2 - index1 - 1)] |>
+                    Seq.distinct |> Seq.toList |>
+                        List.map(fun x -> x |> getInfoAsync)
         Async.Parallel dataList |> Async.RunSynchronously  
         

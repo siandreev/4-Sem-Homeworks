8e2837c6f5963f9576e79cd2339f99d184948726
Sergey <sera200013@gmail.com>
2019-06-07 20:00:57 +0300

    4.3 fixed

--- 4.3.Tests/4.3.Tests.Program.fs
+++ 4.3.Tests/4.3.Tests.Program.fs
@@ -10,14 +10,14 @@
 
     [<Test>]
     let ``check the correctness of the transformation in the case of the simplest case``() =
-        let expression = (Combination(Abstraction('x', Variable('x')), Variable('y')))
+        let expression = (Application(Abstraction('x', Variable('x')), Variable('y')))
         reduction expression |> should equal (Variable('y')) 
 
     [<Test>]
     let ``check the alpha-conversion``() =
         let K = Abstraction('x', Abstraction('y', Variable('x')))
-        let Ky =Combination(K, Variable('y'))
-        let expression = Combination(Abstraction('x', Abstraction('y', Variable('x'))), Variable('y'))
+        let Ky = Application(K, Variable('y'))
+        let expression = Application(Abstraction('x', Abstraction('y', Variable('x'))), Variable('y'))
         reduction Ky |> should equal (Abstraction('a', Variable('y')))
 
     [<Test>]
@@ -26,10 +26,10 @@
             Abstraction(
                 'x', Abstraction(
                     'y', Abstraction(
-                        'z', Combination(
-                            Combination(Variable('x'), (Variable('z'))), 
-                            Combination(Variable('y'), (Variable('z'))))))) 
+                        'z', Application(
+                            Application(Variable('x'), (Variable('z'))), 
+                            Application(Variable('y'), (Variable('z'))))))) 
         let K = Abstraction('x', Abstraction('y', Variable('x')))
-        let SKK = Combination(Combination(S, K), K)
+        let SKK = Application(Application(S, K), K)
         let I = Abstraction('z',Variable('z'))
         reduction SKK |> should equal I
--- 4.3/4.3/4.3.Program.fs
+++ 4.3/4.3/4.3.Program.fs
@@ -1,74 +1,64 @@
 ﻿module Logic
     /// описание типа лямбда-выражение
-    type LambdaTherm =
+    type LambdaTerm =
     | Variable of char
-    | Combination of  LambdaTherm * LambdaTherm
-    | Abstraction of  char * LambdaTherm
+    | Application of  LambdaTerm * LambdaTerm
+    | Abstraction of  char * LambdaTerm
 
     /// функция для получения списка свободных переменных в лямбда-выражении
-    let rec  getFreeVariables (mainExpression : LambdaTherm) =
+    let rec  getFreeVariables (mainExpression : LambdaTerm) =
         let rec fV mainExpression listOfFV =
             match mainExpression with
-            | Variable variable-> variable::listOfFV
-            | Combination(expression1, expression2) -> listOfFV @ getFreeVariables(expression1) @ getFreeVariables(expression2)
+            | Variable variable -> variable :: listOfFV
+            | Application(expression1, expression2) -> listOfFV @ getFreeVariables(expression1) @ getFreeVariables(expression2)
             | Abstraction(variable, expression) -> listOfFV @ (List.filter(fun x -> x <> variable) <| getFreeVariables(expression))
         fV mainExpression []
 
     /// функция для получения нового имени для переменования связной переменной
-    let getNewName (expression1 : LambdaTherm) (expression2 : LambdaTherm) =
+    let getNewName (expression1 : LambdaTerm) (expression2 : LambdaTerm) =
         let listFV1 = getFreeVariables expression1
         let listFV2 = getFreeVariables expression2
         let listFV = listFV1 @ listFV2
         let allSymbolsList = ['a'..'z']
-        let availableSymbilsList = List.filter(fun x -> not(List.contains(x) listFV)) allSymbolsList
-        availableSymbilsList.Head
+        let availableSymbolsList = List.filter(fun x -> not(List.contains(x) listFV)) allSymbolsList
+        availableSymbolsList.Head
         
     /// функция подстановки в mainExpression терма term вместо вхождений переменной mainVariable
     /// (с поддержкой альфа-конверсии в случае, если переменная связана)
     let rec substitution mainExpression mainVariable term =
         match mainExpression with
         | Variable variable when variable = mainVariable -> term
-        | Variable variable  -> LambdaTherm.Variable variable
-        | Combination(expression1, expression2) -> 
-            LambdaTherm.Combination(substitution expression1 mainVariable term, substitution expression2 mainVariable term)
-        | Abstraction(variable,expression) when variable = mainVariable -> mainExpression
-        | Abstraction(variable,expression)  -> 
+        | Variable variable  -> LambdaTerm.Variable variable
+        | Application(expression1, expression2) -> 
+            LambdaTerm.Application(substitution expression1 mainVariable term, substitution expression2 mainVariable term)
+        | Abstraction(variable, expression) when variable = mainVariable -> mainExpression
+        | Abstraction(variable, expression) -> 
             if (not(List.contains(variable) (getFreeVariables term)) || 
                 not(List.contains(mainVariable) (getFreeVariables expression))) then 
-                let (newExpression : LambdaTherm) = substitution expression mainVariable term
+                let (newExpression : LambdaTerm) = substitution expression mainVariable term
                 Abstraction(variable, newExpression)
             else 
                 let newVariable = getNewName mainExpression term
-                let firstSub = substitution expression variable (LambdaTherm.Variable newVariable)
-                let SecondSub = substitution firstSub mainVariable term
-                Abstraction(newVariable, SecondSub)
+                let firstSub = substitution expression variable (LambdaTerm.Variable newVariable)
+                let secondSub = substitution firstSub mainVariable term
+                Abstraction(newVariable, secondSub)
      
     /// бета-редукция лямбда-выражения по нормальной стратегии
-    let rec betaReduction (mainExpression: LambdaTherm) =
+    let rec betaReduction (mainExpression: LambdaTerm) =
         match mainExpression with
-        | Variable variable -> LambdaTherm.Variable variable
+        | Variable variable -> LambdaTerm.Variable variable
         | Abstraction(variable, term) -> Abstraction(variable, betaReduction term)
-        | Combination(Abstraction(variable, expression), term) ->
+        | Application(Abstraction(variable, expression), term) ->
             betaReduction (substitution expression variable term)
-        | Combination(expression1, expression2) ->
+        | Application(expression1, expression2) ->
             let reductionExp1 = betaReduction expression1
             match reductionExp1 with 
             | Abstraction(variable, expression) -> 
-                betaReduction(Combination(reductionExp1, expression2))
+                betaReduction(Application(reductionExp1, expression2))
             | _ ->
                 let reductionExp2 = betaReduction expression2
-                Combination(reductionExp1, reductionExp2)
+                Application(reductionExp1, reductionExp2)
 
     /// нерекурсивная обертка для бета-редукции
     let reduction expression =
         betaReduction expression
-
-
-
-
-
-      
-         
-            
-        
-  

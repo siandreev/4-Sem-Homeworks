ebed57a0d82642b55967e62e70dcd8e8547bd48f
Sergey <sera200013@gmail.com>
2019-06-07 18:24:55 +0300

    5.1. fixed

--- 5.1/5.1/5.1/5.1Program.fs
+++ 5.1/5.1/5.1/5.1Program.fs
@@ -1,25 +1,42 @@
 ﻿module Logic
       
+    /// функция для проверки, правильно ли стоят скобки в строке
     let checkString (str : string) =
-        // создаем список возможных скобок
+        /// создаем список возможных скобок
         let bracketsList  =  ["(" ; ")" ; "{" ; "}" ; "[" ; "]"]
-
-        // создаем список из скобок в формате string в том порядке, в котором они идут в строке
+        let constOpeningBracketsList = ["(" ; "{" ; "["]
+        
+        /// создаем список из скобок в формате string в том порядке, в котором они идут в строке
         let onlyBracketList = (List.filter(fun x -> (List.contains x bracketsList)) (List.map string (Array.toList <| str.ToCharArray())))
 
-        // создаем из этого списка строку
-        let onlyBracketsString = onlyBracketList |> List.fold (+) "" 
-        let length = onlyBracketsString.Length
+        /// функция для получения открывающей скобки по закрывающей
+        let getOpenByClose bracket =
+            match bracket with
+            | ")" -> Some "("
+            | "}" -> Some "{"
+            | "]" -> Some "["
+            | _ -> None
 
-        // функция, которая убирает из строки вхождения пары открывающая-закрывающая скобка. В каждой строке с правильной расстановкой скобок такая пара есть в любой момент
-        let rec reducion (s : string) length =
-            let newStr = s.Replace("()", "").Replace("{}", "").Replace("[]", "")
-            let newLength = newStr.Length
-
-            // сверяем длину строки после удления: если изменилась более чем на 2- все норм, продолжаем вычеркивание до получения либо нулевой строки - тогда true,
-            // либо если в какой-то момент не можем сократить, а строка не пустая- false
-            match newLength with
-            | _ when (length - newLength) >=2 -> reducion newStr newLength
-            | 0 -> true
-            | _ -> false
-        reducion onlyBracketsString length
+        /// проходим по полученному списку из скобок: если встречаем открывающую- кладем ее в начало списка
+        /// открывающих. Если закрывающую- сверяем, что она соответствует верхней скобке в списке открывающих.
+        /// если что-то идет не так- возвращаем false, если успешно обнулили основной и вспомогательный списки- true
+        let rec reducion workBracketsList openBracketsList =
+           match workBracketsList with
+           | [] -> 
+               if openBracketsList = [] then
+                   true
+               else 
+                   false
+           | elem :: tail ->
+               if List.contains(elem) constOpeningBracketsList then
+                   reducion tail (elem :: openBracketsList)
+               else 
+                   match openBracketsList with
+                   | [] -> false
+                   | bracket :: tailOfBracketsList ->
+                       if bracket = (getOpenByClose elem).Value then
+                           reducion tail tailOfBracketsList
+                       else
+                           false
+        reducion onlyBracketList []
+        

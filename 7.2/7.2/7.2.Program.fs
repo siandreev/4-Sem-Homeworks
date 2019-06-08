1e4eb44eb3e325a7f10787e1ec23d41c89c933cb
Sergey <sera200013@gmail.com>
2019-06-08 21:43:59 +0300

    7.2 fixed

--- 7.2/7.2/7.2.Program.fs
+++ 7.2/7.2/7.2.Program.fs
@@ -1,11 +1,20 @@
 ﻿module Logic
 
+    /// функция для преобразования строки в число: в зависимости от рещультата tryParse 
+    /// возвращаем либо Some это число, либо None
+    let stringParser (s : string) =
+        let parseResult = System.Int32.TryParse(s)
+        if  (fst parseResult) then
+            Some (snd parseResult)
+        else 
+            None
+        
+    /// вычисление значения выражения, либо возврат сигнала о том, что оно записано неправильно 
     type StringCalcBuilder() =
         member this.Bind(s : string, f) =
-            try
-                f (int s)
-            with
-                :? System.FormatException -> printf "Wrong data"; None
+            match stringParser s with
+            | None -> None
+            | Some value -> f value
         member this.Return(s) =
             Some(s)
        
